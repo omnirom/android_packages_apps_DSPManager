b68b0c131a7cfae151140cc5926740cb87f23600
JustArchi <JustArchi@JustArchi.net>
2014-05-03 17:34:39 +0200

    Boeffla: Apply values on boot only if nothing applied them before
    This fixes a conflict with Boeffla Config V2 app, when DSP Manager would override already properly set values with other ones
    
    Change-Id: I88c80dbc24a123837a9facf6b87dd5acda02a8d4

--- src/com/bel/android/dspmanager/receiver/BootCompletedReceiver.java
+++ src/com/bel/android/dspmanager/receiver/BootCompletedReceiver.java
@@ -38,7 +38,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (SoundControlHelper.getSoundControlHelper(context).isSupported()) {
             SoundControlHelper.getSoundControlHelper(context).applyValues();
         }
-        if (BoefflaSoundControlHelper.getBoefflaSoundControlHelper(context).getBoefflaSound()) {
+        if (BoefflaSoundControlHelper.getBoefflaSoundControlHelper(context).getBoefflaSound() && BoefflaSoundControlHelper.getBoefflaSoundControlHelper(context).readBoefflaSound() != 1) {
             BoefflaSoundControlHelper.getBoefflaSoundControlHelper(context).applyValues();
         }
     }
