ef4bcb8982ff6812573d5d91912b61af0f8cf64a
Jake Whatley <jacob.whatley91@gmail.com>
2015-12-09 19:17:11 -0500

    DSPManager: add option to ignore DSPManager's audio_effects.conf
    This allows devices to still build DSPManager and not include it's
    packaged audio_effects
    
    This is useful for two reasons:
    
    1) Devices with a vendor partition symlink to system/vendor MUST not
    have that path in $(TARGET_OUT)
    2) Can be used in conjuntion with https://gerrit.omnirom.org/#/c/15718/
    to allow a device to use it's own audio_effects
    
    Change-Id: I299bb564a7fe4d251a008c0d700bd8ae48efaf3c

--- cyanogen-dsp/Android.mk
+++ cyanogen-dsp/Android.mk
@@ -37,6 +37,7 @@ LOCAL_SHARED_LIBRARIES := \
 
 include $(BUILD_SHARED_LIBRARY)
 
+ifneq ($(TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF),true)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audio_effects.conf
@@ -50,3 +51,4 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/etc
 
 include $(BUILD_PREBUILT)
+endif
